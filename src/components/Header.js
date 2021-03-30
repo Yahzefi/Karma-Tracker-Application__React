2a4ed0955e5e0fcf397efe8baef8836a47070f2e
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-03-30 01:13:17 -0400

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,33 @@
-import { useContext } from "react"
+import { useState } from "react"
 
-import { ThemeContext } from "../contexts/themeContext"
+import Navbar from "./Navbar"
+
+import logo from '../Images/header_logo.png'
+
+// import { ThemeContext } from "../contexts/themeContext"
+
+// const Header = () => {
+//     const themes = useContext(ThemeContext)
+//     const [currentTheme, setCurrentTheme] = useState('light')
+//     return (
+//         <div className="h-cont" style={currentTheme === 'light' ? {backgroundColor: themes.light.background, color:themes.light.textColor} : {backgroundColor: themes.dark.background, color: themes.dark.textColor}}>
+//             <h2>Welcome to the Karma Tracker!</h2>
+//             <button 
+//             className="theme-btn" 
+//             style={currentTheme === 'light' ? {backgroundColor: themes.dark.background, color: themes.dark.textColor} : {backgroundColor: themes.light.background, color: themes.light.textColor}}
+//             onClick={currentTheme === 'light' ? ()=>setCurrentTheme('dark') : ()=>setCurrentTheme("light")}
+//             >Change Theme</button>
+//         </div>
+//     )
+// }
 
 const Header = () => {
-    const themes = useContext(ThemeContext)
+    const [logoAnimation, setLogoAnimation] = useState(true)
     return (
-        <div className="h-cont" style={{backgroundColor: themes.dark.background, color:themes.dark.foreground}}>
-            <h2>Welcome to the Karma Tracker!</h2>
+        <div className="h-cont">
+            <Navbar navClick={(e)=>console.log(e.target)}/>
+            <h1 className="h-title">The Karma Tracker</h1>
+            <img src={logo} alt="logo" id="page_logo" className="h-logo" style={logoAnimation ? {animationPlayState: 'running'} : {animationPlayState: 'paused'}} onClick={()=>setLogoAnimation(!logoAnimation)}/>
         </div>
     )
 }
