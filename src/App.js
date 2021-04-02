5a521e990b80111f254e24c20dc6ed82daa7be14
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-04-01 20:49:21 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import './App.css';
 import Feed from './components/Feed';
 import Header from "./components/Header"
+import Scale from './components/Scale';
 import Sidebar from './components/Sidebar';
 // import ThemeContextProvider from './contexts/themeContext';
 
 function App() {
   return (
-    <div>
+    <div className="primary-cont">
       <Header />
-      <Sidebar />
-      <Feed />
+      <div className="scale-cont">
+        <Scale />
+      </div>
+      <div className="body-cont">
+        <Sidebar />
+        <Feed />
+      </div>
     </div>
   );
 }
