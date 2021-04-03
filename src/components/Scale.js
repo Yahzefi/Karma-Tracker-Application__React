3a134f0b7bdbdd46d20c1e8a76f5e04e01a1468b
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-04-03 00:11:59 -0400

    Update Scale.js

--- src/components/Scale.js
+++ src/components/Scale.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Scale = () => {
+    const [karmaStatus, setKarmaStatus] = useState(50)
     return (
         <div className="scale-cont">
             <div className="scale-box">
-                <div className="scale-fill">
+                <div className="scale-fill" style={karmaStatus <= 40 ? {backgroundColor: "red", width: `${karmaStatus}%`} : karmaStatus >= 40 && karmaStatus <= 60 ? {backgroundColor: "yellow", width: `${karmaStatus}%`} : {backgroundColor: 'green', width: `${karmaStatus}%`}}>
                     <p style={{textAlign: "center"}}>Progress</p>
                 </div>
             </div>
