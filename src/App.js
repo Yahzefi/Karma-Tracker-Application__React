2611d2a7e500a40f9a859115f9baafe3de4c90fa
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-04-03 00:10:35 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
+import { useEffect, useState } from "react"
+
 import './App.css';
-import Feed from './components/Feed';
-import Header from "./components/Header"
+
+import Modals from './components/Modals';
+import Header from "./components/Header";
 import Scale from './components/Scale';
 import Sidebar from './components/Sidebar';
+import Feed from './components/Feed';
+import Footer from './components/Footer';
 // import ThemeContextProvider from './contexts/themeContext';
 
 function App() {
+  const [score, setScore] = useState(0)
+  useEffect(()=>{
+    const fetchData = async () => {
+      const module = await import('./localAPI/actions.json')
+      const data = module.Actions
+      console.log("Worth One Point: " + data.Good.One)
+    }
+    fetchData()
+  }, [])
   return (
     <div className="primary-cont">
+      <Modals />
       <Header />
       <div className="scale-cont">
         <Scale />
@@ -16,6 +31,9 @@ function App() {
         <Sidebar />
         <Feed />
       </div>
+      <>
+        <Footer />
+      </>
     </div>
   );
 }
