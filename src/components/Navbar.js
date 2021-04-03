ec3815fde9f794a378c2bf7332726fe7652f26e5
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-04-03 00:11:51 -0400

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,8 +3,8 @@
 const Navbar = ({ navClick }) => {
     return (
         <nav className="h-nav">
-            <span className="nav-item" onClick={navClick}><p>LOGIN</p></span>
-            <span className="nav-item" onClick={navClick}><p>REGISTER</p></span>
+            <span className="nav-item" onClick={navClick}><p id="login">LOGIN</p></span>
+            <span className="nav-item" onClick={navClick}><p id="register">REGISTER</p></span>
         </nav>
     )
 }
