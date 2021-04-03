d231b2571e114da8a789e43d2e45e122d5224029
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-04-03 00:10:03 -0400

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+// import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className="foot-cont">
+            <p style={{fontSize: "64px", color: "white"}}>Footer</p>
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/components/Modals.js
@@ -0,0 +1,11 @@
+// import React from 'react'
+
+const Modals = () => {
+    return (
+        <div style={{display: "none"}}>
+            <p>Modal</p>
+        </div>
+    )
+}
+
+export default Modals
