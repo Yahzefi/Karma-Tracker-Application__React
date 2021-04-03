f9873f0d4a96cf98049110522fef87f8cf2fe653
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-04-03 00:11:39 -0400

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -23,9 +23,23 @@ import logo from '../Images/header_logo.png'
 
 const Header = () => {
     const [logoAnimation, setLogoAnimation] = useState(true)
+
+    const pullModal = (ev) => {
+        switch(ev.target.id){
+            case "login":
+                console.log("login modal")
+                break;
+            case "register":
+                console.log("register modal")
+                break;
+            default:
+                throw Error("Navigation Item Not Found")
+        }
+    }
+
     return (
         <div className="h-cont">
-            <Navbar navClick={(e)=>console.log(e.target)}/>
+            <Navbar navClick={(ev)=>pullModal(ev)}/>
             <h1 className="h-title">The Karma Tracker</h1>
             <img src={logo} alt="logo" id="page_logo" className="h-logo" style={logoAnimation ? {animationPlayState: 'running'} : {animationPlayState: 'paused'}} onClick={()=>setLogoAnimation(!logoAnimation)}/>
         </div>
