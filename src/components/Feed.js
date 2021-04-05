cea0d80b3c5f69888ef8e5ab4bdfa4487dc43f84
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-04-05 09:55:35 -0400

    Update Feed.js

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,27 +1,17 @@
 // import React from 'react'
 import { useLocation } from "react-router-dom"
 
-const Feed = ({ listItem }) => {
+import Section from './Section'
+
+const Feed = ({ actions }) => {
     const location = useLocation()
     return (
         <div className="feed-cont">
-                {/* In here will be a conditional return depending on the current location */}
-                {location.pathname === '/chapter_one' ?
-                <ul style={{listStyle:"none"}}>
-                    {listItem}
-                </ul>
-                : location.pathname === '/chapter_two' ?
-                <h2 style={{color:"white"}}>Chapter Two</h2>
-                : location.pathname === '/chapter_three' ?
-                <h2 style={{color:"white"}}>Chapter Three</h2>
-                : location.pathname === '/chapter_four' ?
-                <h2 style={{color:"white"}}>Chapter Four</h2>
-                : location.pathname === '/chapter_five' ?
-                <h2 style={{color:"white"}}>Chapter Five</h2>
-                : location.pathname === '/chapter_six' ?
-                <h2 style={{color:"white"}}>Chapter Six</h2>
-                : ""
-                }
+            <div className="items-cont">
+{            actions.map((act)=>(
+                        <Section key={act.id} actionNumber={act.id} actionName={act.title} /> 
+                    ))}
+            </div>
         </div>
     )
 }
