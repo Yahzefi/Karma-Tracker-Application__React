c2d9125341057a32db002fd1faf239471dfe806d
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-04-06 00:08:11 -0400

    Update updateChapter.js

--- src/reducers/updateChapter.js
+++ src/reducers/updateChapter.js
@@ -4,6 +4,14 @@ export function updateChapter(state, update){
             return "c1"
         case 2:
             return "c2"
+        case 3:
+            return "c3"
+        case 4:
+            return "c4"
+        case 5:
+            return "c5"
+        case 6:
+            return "c6"
         default:
             return state
     }
