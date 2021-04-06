fd15b670d8cc404404c8fbdaf77dfa757b814c60
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-04-06 00:06:39 -0400

    Update Feed.js

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -3,14 +3,33 @@ import { useLocation } from "react-router-dom"
 
 import Section from './Section'
 
-const Feed = ({ actions }) => {
+const Feed = ({ actions, chapterName, changeScore }) => {
     const location = useLocation()
     return (
         <div className="feed-cont">
+            {location.pathname !== '/' &&
+            <div className="item-head">
+                <h4>Number</h4>
+                <h4 style={{textAlign:"center"}}>Action Name</h4>
+                <h4 style={{textAlign:"center"}}>Location</h4>
+                <h4 style={{textAlign:"right"}}>Done?</h4>
+            </div>
+            }
             <div className="items-cont">
-{            actions.map((act)=>(
-                        <Section key={act.id} actionNumber={act.id} actionName={act.title} /> 
-                    ))}
+                {
+                    actions.map((act)=>(
+                        <Section 
+                            key={`${chapterName}_${act.id}`} 
+                            chapterName={chapterName}
+                            chapterKarma={act.karmaType}
+                            karmaValue={act.value} 
+                            actionNumber={act.id} 
+                            actionName={act.title} 
+                            actionLocation={act.location} 
+                            changeScore={changeScore}
+                        /> 
+                    ))
+                }
             </div>
         </div>
     )
