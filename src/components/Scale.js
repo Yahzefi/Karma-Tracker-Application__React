63835d4399728078bad1f5e21bfe1dbbc3b01dc8
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-04-06 00:07:16 -0400

    Update Scale.js

--- src/components/Scale.js
+++ src/components/Scale.js
@@ -1,14 +1,26 @@
-import { useState } from "react"
 
-const Section = ({ chapterName, actionNumber, actionName }) => {
-    const [isChecked, setChecked] = useState(false)
+const Scale = ({ karmaStatus, currentKarma }) => {
     return (
-        <div className="item-cont" id={chapterName}>
-            <p className="item-num">{actionNumber}</p>
-            <p className="item-name">{actionName}</p>
-            <input className="act-check"  type="checkbox" onChange={()=>setChecked(!isChecked)} checked={isChecked}/>
+        <div className="scale-cont">
+            <div className="scale-num">
+                <span>0</span>
+                <span style={{paddingLeft:"22.5%"}}>25</span>
+                <span style={{textAlign: "center"}}>50</span>
+                <span style={{paddingLeft: "72.5%"}}>75</span>
+                <span style={{textAlign: "right"}}>100</span>
+            </div>
+            <div className="scale-box">
+                <div className="scale-fill" style={karmaStatus <= 40 ? {backgroundColor: "red", width: `${karmaStatus}%`} : karmaStatus >= 40 && karmaStatus <= 60 ? {backgroundColor: "yellow", width: `${karmaStatus}%`} : {backgroundColor: 'green', width: `${karmaStatus}%`}}>
+                    <p style={{textAlign: "center"}}>{currentKarma}</p>
+                </div>
+            </div>
+            <span className="scale-bar bar-0"></span>
+            <span className="scale-bar bar-25"></span>
+            <span className="scale-bar bar-50"></span>
+            <span className="scale-bar bar-75"></span>
+            <span className="scale-bar bar-100"></span>
         </div>
     )
 }
 
-export default Section
+export default Scale
