48bb4773f9dc88fd2ede5de35024aa939171d8d5
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-04-06 00:08:01 -0400

    Update updateActions.js

--- src/reducers/updateActions.js
+++ src/reducers/updateActions.js
@@ -5,6 +5,14 @@ export function updateActions(state, update){
             return update.data.C1
         case "c2":
             return update.data.C2
+        case "c3":
+            return update.data.C3
+        case "c4":
+            return update.data.C4
+        case "c5":
+            return update.data.C5
+        case "c6":
+            return update.data.C6
         default:
             return state
     }
