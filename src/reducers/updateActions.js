3591c62752e46a196701d2317da473379f21c4bb
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-04-13 17:11:10 -0400

    Update updateActions.js

--- src/reducers/updateActions.js
+++ src/reducers/updateActions.js
@@ -1,6 +1,9 @@
 export function updateActions(state, update){
 
     switch(update.type){
+        case "home":
+            state = [];
+            return state;
         case "c1":
             return update.data.C1
         case "c2":
