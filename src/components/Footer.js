cf33e2c86990a660bfb67c0f517c2e4ab4b38ca5
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-04-13 17:09:35 -0400

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,12 +5,27 @@ const Footer = () => {
         <div className="foot-cont">
             <div className="gameLinks-cont">
                 <h4>GAME LINKS</h4>
+                <p>1</p>
+                <p>2</p>
+                <p>3</p>
+                <p>4</p>
+                <p>5</p>
             </div>
             <div className="brand-cont">
                 <h4>SITE LOGO/OTHER BRANDING</h4>
+                <p>1</p>
+                <p>2</p>
+                <p>3</p>
+                <p>4</p>
+                <p>5</p>
             </div>
             <div className="contact-cont">
                 <h4>SOCIAL MEDIA LINKS</h4>
+                <p>1</p>
+                <p>2</p>
+                <p>3</p>
+                <p>4</p>
+                <p>5</p>
             </div>
         </div>
     )
