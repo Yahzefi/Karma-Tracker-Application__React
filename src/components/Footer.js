18cdb0c6fa5bcb3cc7a9a3aeb5db61b21a38cd09
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-04-14 11:18:43 -0400

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,31 +1,26 @@
 // import React from 'react'
+import logo from '../Images/header_logo.png';
 
 const Footer = () => {
     return (
         <div className="foot-cont">
-            <div className="gameLinks-cont">
-                <h4>GAME LINKS</h4>
-                <p>1</p>
-                <p>2</p>
-                <p>3</p>
-                <p>4</p>
-                <p>5</p>
+            <div className="gameLinks-cont" >
+                <h4 style={{marginBottom: "5%"}}>GAME LINKS</h4>
+                <a href="#">Fallout 3</a>
+                <a href="#">Fallout New Vegas</a>
+                <a href="#">Fallout 4</a>
+                <a href="#">Fallout 76</a>
             </div>
             <div className="brand-cont">
-                <h4>SITE LOGO/OTHER BRANDING</h4>
-                <p>1</p>
-                <p>2</p>
-                <p>3</p>
-                <p>4</p>
-                <p>5</p>
+                <img src={logo} className="foot-logo"/>
+                <p>&copy; Karma Tracker</p>
             </div>
             <div className="contact-cont">
-                <h4>SOCIAL MEDIA LINKS</h4>
-                <p>1</p>
-                <p>2</p>
-                <p>3</p>
-                <p>4</p>
-                <p>5</p>
+                <h4 style={{marginBottom: "5%"}}>SOCIAL MEDIA LINKS</h4>
+                    <a href="#">Facebook</a>
+                    <a href="#">Twitter</a>
+                    <a href="#">Instagram</a>
+                    <a href="#">YouTube</a>
             </div>
         </div>
     )
